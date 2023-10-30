17c5eb53771974fc6c90a1e3ddeea107552751b7
RyGuy-12345 <m262016@usna.edu>
2023-10-29 20:00:41 -0400

    did more stuff

---
+++ Miners.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+from gameparameters import *
+
+#initialize pygame
+pygame.init()
+
+mine = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("the children yern for the mines")
+
