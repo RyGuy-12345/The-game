2c7cc895481fde3a6c13351d60033ec2efc6279c
RyGuy-12345 <m262016@usna.edu>
2023-10-30 11:16:50 -0400

    tried to blit, unsucessfully

--- display.py
+++ display.py
@@ -8,3 +8,8 @@ def draw_background(mine):
     dirt = pygame.image.load("../assets/sprites/dirt.PNG")
     top_soil = pygame.image.load("../assets/sprites/top_soil.png")
     top_soil.set_colorkey((0, 0, 0))
+    dirt.set_colorkey((0, 0, 0))
+
+    for x in range(0,screen_width,tile_size):
+        for y in range(0,screen_height, tile_size):
+            mine.blit(dirt, (x,y))
