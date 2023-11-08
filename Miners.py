cd423852ee1715c97b4891f88a2898bfeb9d42ec
RyGuy-12345 <m262016@usna.edu>
2023-11-08 11:46:40 -0500

    did some more stuff

--- Miners.py
+++ Miners.py
@@ -8,3 +8,16 @@ pygame.init()
 mine = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("the children yern for the mines")
 
+#set clock
+clock = pygame.time.Clock()
+
+running = True
+background = screen.copy()
+draw_background(background)
+
+while running:
+    for event in pygame.event.get():
+        #print(event)
+        if event.type == pygame.QUIT:
+            running = False
+    screen.blit(background, (0, 0))
