c593b97489509c89c9ac881b914b7a95b1f366a4
RyGuy-12345 <m262016@usna.edu>
2023-11-22 01:46:03 -0500

    made a second player

--- MINER/Miners.py
+++ MINER/Miners.py
@@ -1,11 +1,19 @@
 import time
-
 import pygame
 import sys
 from gameparameters import *
 from display import *
 from diamonds import *
-from player import Player
+from player import *
+
+#make a main menu
+#fix the movement bug
+#make a closing menu
+#make dirt change color once dug
+#make sprites spawn on upper left and right corners of dirt
+#stop diamonds from spawning in the sky
+#make dinamite
+
 
 #initialize pygame
 pygame.init()
@@ -27,52 +35,93 @@ for _ in range(0, num_diamonds):
     diamond_list.append(Diamonds(mine))
 
 #miner
-player = Player(screen_width/2, screen_height/2)
+player1 = Player1(screen_width/2, screen_height/2)
+p1_score = 0
+player2 = Player2(screen_width/2, screen_height/2)
+p2_score = 0
 
-
-
-while running:
+#when the list is empty set running to false
+while running and diamond_list:
     for event in pygame.event.get():
         #print(event)
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                player.move_up()
+                player1.move_up()
             if event.key == pygame.K_DOWN:
-                player.move_down()
+                player1.move_down()
             if event.key == pygame.K_LEFT:
-                player.move_left()
+                player1.move_left()
             if event.key == pygame.K_RIGHT:
-                player.move_right()
+                player1.move_right()
+            if event.key == pygame.K_w:
+                player2.move_up()
+            if event.key == pygame.K_s:
+                player2.move_down()
+            if event.key == pygame.K_a:
+                player2.move_left()
+            if event.key == pygame.K_d:
+                player2.move_right()
         if event.type == pygame.KEYUP:
-            player.stop()
+            if event.type == pygame.K_UP or pygame.K_LEFT or pygame.K_DOWN or pygame.K_RIGHT:
+                player1.stop()
+            if event.type == pygame.K_a or pygame.K_d or pygame.K_s or pygame.K_w:
+                player2.stop()
+## this idea did not work
+            #update to make only one player stop K_A OR K_S OR K_d
+
+    if diamond_list == 0:
+        pygame.quit()
+
 
 #print mine
     mine.blit(background, (0, 0))
 #update miner
-    player.update()
-    player.draw(mine)
+    player1.update()
+    player1.draw(mine)
+    player2.update()
+    player2.draw(mine)
+    idx1 = 0
+    idx2 = 0
 #print diamonds
     for diamond in diamond_list:
         diamond.draw_diamonds(mine)
-        result = pygame.sprite.collide_rect(player, diamond)
-        print(result)
-        if result:
-            diamond_list.pop()
+        result1 = pygame.sprite.collide_rect(player1, diamond)
+        result2 = pygame.sprite.collide_rect(player2, diamond)
+        if result1:
+            diamond_list.pop(idx1)
+            p1_score += 1
         #pop when collide with character
+        if idx1 == len(diamond_list):
+            idx1 = 0
+        else:
+            idx1 += 1
+
+        if result2:
+            diamond_list.pop(idx2)
+            p2_score += 1
+        if idx2 == len(diamond_list):
+            idx2 = 0
+        else:
+            idx2 += 1
+
     pygame.display.flip()
 
-score = len(diamond_list)
+
 
 #create screen for game over
+
 mine.blit(background, (0, 0))
-font = pygame.font.Font("../assets/fonts/Montague.ttf", 115)
+font = pygame.font.Font("../assets/fonts/Montague.ttf", 100)
 message = font.render("GAME OVER", True, (0, 0, 0))
-mine.blit(message, (screen_width/2 - message.get_width()/2, screen_height/2))
-score_text = font.render(f'Score: {score}', True, (0, 0, 0))
-mine.blit(score_text, (screen_width/2 - score_text.get_width()/2, screen_height/2 +score_text.get_height()))
-time.sleep(2)
+mine.blit(message, (screen_width/2 - message.get_width()/2, screen_height/6))
+score1_text = font.render(f'PLAYER 1 SCORE: {p1_score}', True, (0, 0, 0))
+mine.blit(score1_text, (screen_width/2 - score1_text.get_width()/2, screen_height/4 +score1_text.get_height()))
+score2_text = font.render(f'PLAYER 2 SCORE: {p2_score}', True, (0, 0, 0))
+mine.blit(score2_text, (screen_width/2 - score2_text.get_width()/2, screen_height/2 +score2_text.get_height()))
 pygame.display.flip()
+time.sleep(5)
+pygame.quit()
 
 
--- MINER/player.py
+++ MINER/player.py
@@ -2,7 +2,7 @@ import pygame
 from gameparameters import *
 
 
-class Player(pygame.sprite.Sprite):
+class Player1(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         self.imagebackward = pygame.image.load('../assets/sprites/miner.PNG').convert()
@@ -58,3 +58,61 @@ class Player(pygame.sprite.Sprite):
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+class Player2(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.imagebackward = pygame.image.load('../assets/sprites/miner2.png').convert()
+        self.imageforward = pygame.image.load('../assets/sprites/miner2.png').convert()
+        self.imagebackward = pygame.transform.flip(self.imageforward, True, False)
+        self.imageforward.set_colorkey((0, 0, 0))
+        self.imagebackward.set_colorkey((0, 0, 0))
+        self.image = self.imageforward
+        self.rect = self.imageforward.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.center = (x, y)
+        self.x_speed = 0
+        self.y_speed = 0
+
+    def move_up(self):
+       self.y_speed = -1*PLAYER_SPEED
+
+    def move_down(self):
+        self.y_speed = PLAYER_SPEED
+
+    def move_left(self):
+        self.x_speed = -1*PLAYER_SPEED
+        self.image = self.imageforward
+
+    def move_right(self):
+        self.x_speed = PLAYER_SPEED
+        self.image = self.imagebackward
+
+
+    def stop(self):
+        self.x_speed = 0
+        self.y_speed = 0
+
+    def update(self):
+        #need to stop player if it goes off screen
+        self.x += self.x_speed
+        self.y += self.y_speed
+        if self.x > screen_width-tile_size:
+            self.x = screen_width-tile_size
+        if self.x < 0:
+            self.x = 0
+        if self.y > screen_height-tile_size:
+            self.y = screen_height-tile_size
+        if self.y < 0:
+            self.y = 0
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+
+
+
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
